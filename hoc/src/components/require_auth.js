ec8287473df2ff34172dea57b2595d52e539d2eb
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-17 19:15:56 +0100

    Gain external access in Authentication HOC
    Connect to the Redux store for the authenticated status.
    Connect to the router in the passed context

--- hoc/src/components/require_auth.js
+++ hoc/src/components/require_auth.js
@@ -1,14 +1,27 @@
-import React            from 'react';
+import React, { Component }   from 'react';
+import { connect }            from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+    
     render() {
+      console.log(this.context);
+//      console.log('Authenticated:', this.props.authenticated);
+
       return <ComposedComponent {...this.props} />
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Use of this HOC:
@@ -19,3 +32,4 @@ export default function(ComposedComponent) {
 // const ComposedComponent = Authentication(ComponentToWrap);
 
 // <ComposedComponent />
+///////////////////////////////////////////////////////////////////////////
