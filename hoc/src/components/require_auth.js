21d5c64bf1ea765e2fd0e406c4219846eef5cd9a
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-18 21:18:13 +0100

    Complete Authentication HOC, redirecting when not logged in

--- hoc/src/components/require_auth.js
+++ hoc/src/components/require_auth.js
@@ -3,12 +3,26 @@ import { connect }            from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    // Get access to parts of the context, the router in this case
     static contextTypes = {
       router: React.PropTypes.object
     };
-    
+
+    // This is the key here, return to the root if not authenticated
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    // Called when state changes
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      console.log(this.context);
 //      console.log('Authenticated:', this.props.authenticated);
 
       return <ComposedComponent {...this.props} />
