767979aed80afd5a94c5e93878e64fb375c78576
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-22 15:33:29 +0100

    Encrypt password and generate JWT

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,49 @@
+const jwt     = require('jwt-simple');
+
+const User    = require('../models/user');
+const config  = require('../config')
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
 exports.signup = function(req, res, next) {
-  res.send({ success: true });
+  // console.log(req.body);
+
+  const email     = req.body.email;
+  const password  = req.body.password;
+
+  // Ensure that the mandatory data is present
+
+  if(!email || !password) {
+    return res.status(422).send({ error: "You must supply both an email address and password" })
+  }
+
+  // See if a user with the given email already exists
+
+  User.findOne({ email: email }, function(err, foundUser) {
+    // Pass on error(s)
+    if(err) { return next(err); }
+
+    // Return an error if so
+
+    if(foundUser) {
+      return res.status(409).send({ error: "That email address is already in use." });    // Conflicting user
+    }
+
+    // Create and save a new user record
+
+    const newUser = new User({
+      email:    email,
+      password: password
+    });
+
+    newUser.save(function(err) {
+      if(err) { return next(err); }
+
+      res.send({ token: tokenForUser(newUser) });
+    });
+  });
 };
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose    = require('mongoose');
+const bcrypt      = require('bcrypt-nodejs');
 
 const Schema      = mongoose.Schema;
 
@@ -9,10 +10,33 @@ const userSchema = new Schema({
   password: String
 });
 
+// Hook on save() and encrypt password
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  // Generate a salt
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) { return next(err); }
+
+    // Hash the pssword, using the salt
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) { return next(err); }
+
+      // Overwrite the plain-text with the encrypted password
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 // Create the model class
 
 const ModelClass = mongoose.model('user', userSchema);
 
 // Export the model
 
-modules.exports = ModelClass;
+module.exports = ModelClass;
