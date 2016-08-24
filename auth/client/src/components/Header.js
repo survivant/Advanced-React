c4cedfaea7b4414d17894219883262dbcaf197d8
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-24 10:45:20 +0100

    Update header to show logout or login/signup links based on auth

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,17 +1,45 @@
 import React, { Component }         from 'react';
+import { connect }                  from 'react-redux'
+import { Link }                     from 'react-router';
 
 class Header extends Component {
+  renderInOutLinks() {
+    if(this.props.logged_in) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/logout">Log out</Link>
+        </li>
+      );
+    }
+    else {
+      return [(
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/login">Log in</Link>
+        </li>
+      ),
+      (
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign up</Link>
+        </li>
+      )
+      ];
+    }
+  }
+
   render() {
     return (
-      <nav className="navbar navbar-light">
+      <nav className="navbar navbar-light bg-faded">
+        <Link to="/" className="navbar-brand">Authentication</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Log in
-          </li>
+          {this.renderInOutLinks()}
         </ul>
       </nav>
     );
   }
 };
 
-export default Header;
+function mapStateToProps(state) {
+  return { logged_in: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
