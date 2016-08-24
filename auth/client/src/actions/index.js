1e43c9d61be20dbd06455400f6116769485934a6
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-24 16:27:15 +0100

    Add action creator for signing up a new user

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,7 +1,7 @@
 import { browserHistory } from 'react-router';
 import axios              from 'axios';
 
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR }  from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, SIGNUP_USER }  from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -28,8 +28,27 @@ export function loginUser({ email, password }) {
 
 export function logoutUser() {
   localStorage.removeItem('token');
-  
+
   return { type: UNAUTH_USER };
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email and password to server signup path
+
+    axios.post(`${ROOT_URL}/signup`, { email, password }).then(response => {
+      // Update state to indicate that the user is authenticated
+      // Save the returned JWT
+      // Redirect to /feature
+
+      dispatch({ type: AUTH_USER });
+      localStorage.setItem('token', response.data.token);
+      browserHistory.push('/feature');
+    }).catch(error => {
+      console.log(error.response);
+      dispatch(authError(error.response.data.error));
+    });
+  };
 }
 
 export function authError(error) {
