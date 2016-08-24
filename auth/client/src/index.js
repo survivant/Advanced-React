b4c9633b6e9d50b39e0bf54098e5f24f26dbdd86
Julian Nicholls <juliannicholls29@gmail.com>
2016-08-24 17:13:29 +0100

    Log the user in automatically if the token is present

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -7,18 +7,31 @@ import { Router, Route, IndexRoute, browserHistory }  from 'react-router'
 
 import App               from './components/app';
 import reducers          from './reducers';
+import Welcome           from './components/Welcome'
 import LoginForm         from './components/auth/LoginForm'
 import Logout            from './components/auth/Logout'
 import SignupForm        from './components/auth/SignupForm'
 import Feature           from './components/Feature'
 import requireAuth       from './components/auth/require_auth'
 
+import { AUTH_USER }     from './actions/types';
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store                     = createStoreWithMiddleware(reducers);
+
+// If the token is present, the user is already logged in
+
+const token = localStorage.getItem('token');
+
+if(token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="login" component={LoginForm} />
         <Route path="logout" component={Logout} />
         <Route path="signup" component={SignupForm} />
